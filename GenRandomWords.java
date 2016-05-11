87ec14548a711877205e322ecbfadeda083773a9
Thomas Jakway <tjakway@nyu.edu>
2016-05-11 07:56:14 -0400

    initial commit

---
+++ GenRandomWords.java
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define WORDS_PATH "/usr/share/dict/words"
+
+//#1: read in all the words
+//#2: find the longest word
+//#3: the range for generated strings is (1, length(longest word))
+//#4: generate a string
+//#5: check if it's a word
+//#6: log count
+//#7: find percent of words generated in X number of 
